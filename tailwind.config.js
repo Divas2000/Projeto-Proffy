f4166670d58e2be1b15dcfe36f35ec521883c629
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-08-02 11:33:18 -0300

    chore: setting tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,71 +1,69 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin");
-
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
-        display: ["var(--font-sf)", "system-ui", "sans-serif"],
-        default: ["var(--font-inter)", "system-ui", "sans-serif"],
+        archivo: ['var(--font-archivo)'],
+        poppins: ['var(--font-poppins)'],
       },
-      animation: {
-        // Fade up and down
-        "fade-up": "fade-up 0.5s",
-        "fade-down": "fade-down 0.5s",
-        // Tooltip
-        "slide-up-fade": "slide-up-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
-        "slide-down-fade": "slide-down-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
+      fontSize: {
+        lg: ['1.25rem', '1.75rem'],
+        xl: ['1.5rem', '2rem'],
+        '2xl': ['2.25rem', '2.5rem'],
+        '3xl': ['3.5rem', '1']
       },
-      keyframes: {
-        // Fade up and down
-        "fade-up": {
-          "0%": {
-            opacity: 0,
-            transform: "translateY(10px)",
-          },
-          "80%": {
-            opacity: 0.6,
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translateY(0px)",
-          },
+      borderRadius: {
+        DEFAULT: '.5rem',
+        'rounded-t': {
+          'border-top-left-radius': '.5rem',
+          'border-top-right-radius': '.5rem',
         },
-        "fade-down": {
-          "0%": {
-            opacity: 0,
-            transform: "translateY(-10px)",
-          },
-          "80%": {
-            opacity: 0.6,
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translateY(0px)",
-          },
-        },
-        // Tooltip
-        "slide-up-fade": {
-          "0%": { opacity: 0, transform: "translateY(6px)" },
-          "100%": { opacity: 1, transform: "translateY(0)" },
-        },
-        "slide-down-fade": {
-          "0%": { opacity: 0, transform: "translateY(-6px)" },
-          "100%": { opacity: 1, transform: "translateY(0)" },
-        },
+        'rounded-b': {
+          'border-bottom-left-radius': '.5rem',
+          'border-bottom-right-radius': '.5rem',
+        }
       },
+      boxShadow: {
+        sm: '0px 4px 4px rgba(0, 0, 0, .25)',
+      },
+      colors: {
+        white: '#FFFFFF',
+        gray: {
+          50: '#FAFAFC',
+          100: '#F0F0F7',
+          200: '#DCDCE5',
+          300: '#B0ABBC',
+          400: '#9C98A6',
+          600: '#6A6180',
+        },
+        purple: {
+          100: '#D4C2FF',
+          300: '#A380F6',
+          400: '#9871F5',
+          500: '#774DD6',
+          600: '#6842C2',
+          900: '#32264D',
+        },
+        pink: {
+          300: '#E966A0',
+          400: '#E6468B',
+        },
+        red: {
+          400: '#E14C4C',
+        }
+      }
     },
   },
+  daisyui: {
+    themes: []
+  },
   plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-    plugin(({ addVariant }) => {
-      addVariant("radix-side-top", '&[data-side="top"]');
-      addVariant("radix-side-bottom", '&[data-side="bottom"]');
-    }),
+    require('@tailwindcss/typography'), 
+    require('daisyui'),
   ],
-};
+}
