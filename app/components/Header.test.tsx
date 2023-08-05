56b12f35ee83b869ab8b658cd92c2777bce7c163
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-08-05 11:46:20 -0300

    style: updating code formatting

--- app/components/Header.test.tsx
+++ app/components/Header.test.tsx
@@ -1,12 +1,10 @@
-import { render } from "@testing-library/react";
-import Header from "./Header";
+import { render } from '@testing-library/react';
+import Header from './Header';
 
-const TITLE = 'Header'
+const TITLE = 'Header';
 
-
-
-describe("Header Component", () => {
-  test("should render header title", () => {
+describe('Header Component', () => {
+  test('should render header title', () => {
     const { queryByText } = render(<Header/>);
 
     expect(queryByText(TITLE)).toBeInTheDocument();
