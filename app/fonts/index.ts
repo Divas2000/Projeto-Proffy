c8b37962c7b9361746fbdd12c29ae3792a08453f
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-08-24 10:46:20 -0300

    style: formatting the files

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -1,13 +1,15 @@
-import { Archivo, Poppins } from 'next/font/google';
+import { Archivo, Poppins } from "next/font/google";
 
 export const archivo = Archivo({
-  weight: ['600', '700'],
-  subsets: ['latin'],
-  variable: '--font-archivo',
+  weight: ["600", "700"],
+  subsets: ["latin"],
+  variable: "--font-archivo",
+  display: "swap"
 });
 
 export const poppins = Poppins({
-  weight: ['400', '600'],
-  subsets: ['latin'],
-  variable: '--font-poppins',
+  weight: ["400", "600"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  display: "swap"
 });
