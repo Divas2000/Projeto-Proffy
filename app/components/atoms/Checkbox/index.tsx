0691fdf8d074576434af76628ff7681f6ccd9432
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-08-26 15:29:23 -0300

    feat: creating checkbox component

---
+++ app/components/atoms/Checkbox/index.tsx
@@ -0,0 +1,28 @@
+import * as RadixCheckbox from "@radix-ui/react-checkbox";
+import { Check } from "lucide-react";
+
+interface CheckboxProps {
+  /** Checkbox id. */
+  id: string;
+  /** Checkbox label. */
+  label: string;
+}
+
+export function Checkbox({ id, label }: CheckboxProps) {
+  return (
+    <div className="flex gap-4">
+      <RadixCheckbox.Root
+        id={id}
+        className="flex w-6 items-center justify-center rounded border border-gray-200 bg-gray-50 transition-all aria-checked:border-pink-300 aria-checked:bg-pink-300"
+      >
+        <RadixCheckbox.Indicator>
+          <Check color="#fff" size={12} strokeWidth={4} />
+        </RadixCheckbox.Indicator>
+      </RadixCheckbox.Root>
+
+      <label htmlFor={id} className="cursor-pointer font-poppins text-gray-400">
+        {label}
+      </label>
+    </div>
+  );
+}
