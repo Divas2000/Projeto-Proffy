1fab59c9d0b5f553a75166d201b86e4bed47ea49
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-09-08 19:45:27 -0300

    refactor: removing poppins font from next/fonts and importing from google fonts

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -1,14 +1,8 @@
-import { Archivo, Poppins } from "next/font/google";
+import { Archivo } from "next/font/google";
 
 export const archivo = Archivo({
   weight: ["600", "700"],
   subsets: ["latin"],
   variable: "--font-archivo",
-  display: "swap"
-});
-
-export const poppins = Poppins({
-  weight: ["400", "600"],
-  subsets: ["latin"],
-  variable: "--font-poppins",
+  display: "swap",
 });
