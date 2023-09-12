a9b22b110baf6072f41e00686784d1bc187a1c34
ananuness <beatriznunes601@gmail.com>
2023-09-12 17:01:15 -0300

    fix: removing checkbox and use client to import all components except client ones

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,7 +1,6 @@
-"use client";
-
-export * from "./atoms/Checkbox/Checkbox";
+export * from "./atoms/Button/Button";
 export * from "./atoms/HomeButton/HomeButton";
+export * from "./atoms/IconButton/IconButton";
 export * from "./atoms/Label/Label";
 export * from "./atoms/Textarea/Textarea";
 export * from "./atoms/VariableBorderInput/VariableBorderInput";
