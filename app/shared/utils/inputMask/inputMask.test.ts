0ae019960171b0ba0ee968302c3fbcec41e9f135
ananuness <beatriznunez601@gmail.com>
2023-09-16 22:26:41 -0300

    test: adding currencyMask function tests

--- app/shared/utils/inputMask/inputMask.test.ts
+++ app/shared/utils/inputMask/inputMask.test.ts
@@ -1,17 +1,28 @@
-import { onlyLettersMask, onlyNumbersMask, phoneMask, timeMask } from ".";
+import {
+  currencyMask,
+  onlyLettersMask,
+  onlyNumbersMask,
+  phoneMask,
+  timeMask,
+} from "./inputMask";
 
 describe("inputMask Utils", () => {
   test.each([
     ["onlyNumbersMask", onlyNumbersMask],
     ["phoneMask", phoneMask],
     ["timeMask", timeMask],
-  ])("it should return only numbers from %s function", (_, fn) => {
+    ["currencyMask", currencyMask],
+  ])("it should return only numbers from %s function", (variant, fn) => {
     const returnedValue = fn(`
       The quick brown fox jumps over the lazy dog
       !@#$%^&()_+{}[]|:;<>,\\.?/~\`'"-=
     `);
 
-    expect(returnedValue).toBe("");
+    if (variant.includes("currency")) {
+      expect(returnedValue).toBe("R$ ");
+    } else {
+      expect(returnedValue).toBe("");
+    }
   });
 
   test("it should return the formatted phone number from phoneMask function", () => {
@@ -24,6 +35,12 @@ describe("inputMask Utils", () => {
     const returnedValue = timeMask("0845");
 
     expect(returnedValue).toBe("08:45");
+  });
+
+  test("it should return the formatted currency from currencyMask function", () => {
+    const returnedValue = currencyMask("12568");
+
+    expect(returnedValue).toBe("R$ 125,68");
   });
 
   test("it should return only letters from onlyLetters function", () => {
