776ec6510e8424216b39f5f36f73c3aa40ddec7b
ananuness <beatriznunez601@gmail.com>
2023-09-23 15:11:35 -0300

    chore: adding matchMedia mock

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,9 +1,12 @@
 import "@testing-library/jest-dom/extend-expect";
 
 /**
- * JSDOM doesn't implement PointerEvent so we need to mock our own implementation
- * Default to mouse left click interaction
+ * JSDOM doesn't implement PointerEvent so we need to mock 
+ * our own implementation. Default to mouse left click 
+ * interaction.
+ * 
  * https://github.com/radix-ui/primitives/issues/1822
+ * 
  * https://github.com/jsdom/jsdom/pull/2666
  */
 class MockPointerEvent extends Event {
@@ -24,3 +27,24 @@ window.PointerEvent = MockPointerEvent as any;
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
 window.HTMLElement.prototype.releasePointerCapture = jest.fn();
 window.HTMLElement.prototype.hasPointerCapture = jest.fn();
+
+/**
+ * Jest uses JSDOM to create a browser environment. However 
+ * it doesn't support `window.matchMedia`, so creating
+ * MatchMedia mock solves the related problems.
+ * 
+ * https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+ */
+Object.defineProperty(global, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
