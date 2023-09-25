dadfaae396b50142087078dd22453582daab96c0
ananuness <beatriznunez601@gmail.com>
2023-09-24 21:42:41 -0300

    refactor: improving textarea height logic

--- app/components/atoms/Textarea/Textarea.tsx
+++ app/components/atoms/Textarea/Textarea.tsx
@@ -8,15 +8,20 @@ interface TextareaProps extends ComponentProps<"textarea"> {
   label: string;
   /** An optional helper text for the textarea label. */
   helperLabel?: string;
-  /** An optional height in **pixels** for the textarea */
-  height?: number;
+  /**
+   * An optional height for the textarea. If a `number`
+   * is informed, then it will be in **pixels**.
+   *
+   * If not informed, the default value is `fit-content`.
+   */
+  height?: number | string;
 }
 
 export const Textarea = ({
   id,
   label,
   helperLabel,
-  height,
+  height = "fit-content",
   ...props
 }: TextareaProps) => {
   return (
@@ -26,7 +31,9 @@ export const Textarea = ({
         {...props}
         id={id}
         placeholder={label}
-        style={{ height: height ? `${height}px` : "fit-content" }}
+        style={{
+          height: typeof height === "number" ? `${height}px` : height,
+        }}
         className="mt-2 min-h-[58px] w-full resize-y rounded border border-gray-200 bg-gray-50 p-4 font-poppins text-base text-gray-600 placeholder:opacity-0 focus:border-purple-600 focus:outline-none"
       ></textarea>
     </>
