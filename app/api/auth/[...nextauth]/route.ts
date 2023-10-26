01091fa867b9f7a70db95414418510255a945ba0
ananuness <beatriznunez601@gmail.com>
2023-10-25 21:55:26 -0300

    feat: adding login flow management draft

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,58 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from 'prisma';
-import GoogleProvider from 'next-auth/providers/google';
+import prisma from "prisma";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+// type CustomUser = DefaultUser & {
+//   lastName?: string;
+// }
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "E-mail",
+          type: "text",
+          placeholder: "Your e-mail",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Your password",
+        },
+      },
+      async authorize(credentials) /*: Promise<CustomUser | null>*/ {
+        const response = await fetch("/your/endpoint", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(credentials),
+        });
+
+        const user = await response.json();
+
+        if (response.ok && user) return user;
+
+        return null;
+      },
     }),
   ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        Object.defineProperty(session.user, "id", user.id);
+      }
+
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
